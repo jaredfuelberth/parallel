a16549c93a86714e7690e6d8579f0e23d0382e64
Jared Fuelberth <jaredfuelberth@huskers.unl.edu>
2019-12-10 14:02:30 -0600

    Add backend serializer for meter data

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from api.models import OriginalMeterData
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +14,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class MeterDataSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OriginalMeterData
+        fields = ['geom']
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User, Group
+from api.models import OriginalMeterData
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import UserSerializer, GroupSerializer, MeterDataSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,3 +22,11 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class MeterDataViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    permission_classes = ()
+    queryset = OriginalMeterData.objects.all()
+    serializer_class = MeterDataSerializer
