637e3b6dc9e12264326455f3f7b7dcf2cd65ad25
Jared Fuelberth <jaredfuelberth@huskers.unl.edu>
2019-12-09 21:34:06 -0600

    Update backend admin.py

--- api/admin.py
+++ api/admin.py
@@ -9,4 +9,4 @@ from .models import OriginalMeterData
 
 @admin.register(OriginalMeterData)
 class OriginalMeterDataAdmin(OSMGeoAdmin):
-    list_display = ('id', 'e_key', 'type', 'meter_num', 'geom')
+    list_display = ('id', 'e_key', 'type', 'meter_num', 'geom', 'post_num', 'park_rate', 'a_st', 'b_x', 'c_side', 'd_n', 'space', 'globalid')
